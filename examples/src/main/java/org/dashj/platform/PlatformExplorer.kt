b4497b7ce31431c3f9f0299c5799436c413985aa
HashEngineering <hashengineeringsolutions@gmail.com>
2024-03-22 07:32:47 -0700

    feat: update PlatformExplorer example

--- examples/src/main/java/org/dashj/platform/PlatformExplorer.kt
+++ examples/src/main/java/org/dashj/platform/PlatformExplorer.kt
@@ -1,6 +1,9 @@
 package org.dashj.platform
 
 import org.bitcoinj.core.Base58
+import org.dashj.platform.sdk.Identifier
+import org.dashj.platform.sdk.Identity_Tag
+import org.dashj.platform.sdk.Result_ok_dpp_identity_identity_Identity_err_String
 import java.util.Scanner
 import org.dashj.platform.sdk.example;
 
@@ -27,6 +30,23 @@ object PlatformExplorer {
 
         println(" > $idString")
 
+        val value = example.fetchIdentity3(Identifier(Base58.decode(idString)));
+        if (value.ok != null) {
+            when (value.ok.tag) {
+                Identity_Tag.IdentityV0Type -> {
+                    println("id: ${value.ok.v0.id}")
+                    println("balance: ${value.ok.v0.balance}")
+                    println("keys: ${value.ok.v0.publicKeyCount}")
+                }
+                else -> {
+
+                }
+            }
+        } else {
+            println("fetch identity error: ${value.error}")
+        }
 
     }
 }
+
+// MTMoBVf6N9zpPwCTQ51vfttFYmHwfacWUHFupFTApUG
