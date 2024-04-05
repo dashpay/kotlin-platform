768e6602f8349c4c69e5acc9628d22e7105da779
HashEngineering <hashengineeringsolutions@gmail.com>
2024-04-05 10:55:53 -0700

    chore: add stripped down simplified android project

--- dashj-platform-android-simple/src/main/rust/src/lib.rs
+++ dashj-platform-android-simple/src/main/rust/src/lib.rs
@@ -1,9 +1,14 @@
 //mod fermented;
 
 
-use rs_sdk::dpp::identity::IdentityV0;
-use rs_sdk::platform::Identity;
+//use rs_sdk::dpp::identity::IdentityV0;
+//use rs_sdk::platform::Identity;
 
-pub fn get_default_identity() -> Identity {
-    Identity::V0(IdentityV0::default())
-}
+//pub fn get_default_identity() -> Identity {
+//    Identity::V0(IdentityV0::default())
+//}
+
+// use bls_signatures::PrivateKey;
+// pub fn get_secret_key() -> PrivateKey {
+//     PrivateKey::FromBytes();
+// }
