c9fbaa482b62e60fc9a80bb8fd404231ab52359e
HashEngineering <hashengineeringsolutions@gmail.com>
2024-04-11 05:24:28 -0700

    fix: update publishing to maven and include fermented bindings

--- dashj-platform-android/src/main/rust/src/lib.rs
+++ dashj-platform-android/src/main/rust/src/lib.rs
@@ -1,9 +1,9 @@
-//mod fermented;
+mod fermented;
 
 
 use rs_sdk::dpp::identity::IdentityV0;
 use rs_sdk::platform::Identity;
 
-pub fn do_it() -> Identity {
+pub fn dummy_function() -> Identity {
     Identity::V0(IdentityV0::default())
 }
