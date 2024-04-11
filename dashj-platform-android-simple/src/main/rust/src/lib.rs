cdbc64f7c3219705fe3a58bd3dd7dfa50ca4aa59
HashEngineering <hashengineeringsolutions@gmail.com>
2024-04-10 17:17:33 -0700

    chore: changes for successful build of rs-sdk

--- dashj-platform-android-simple/src/main/rust/src/lib.rs
+++ dashj-platform-android-simple/src/main/rust/src/lib.rs
@@ -1,6 +1,38 @@
 //mod fermented;
 
+use std::env;
 
+pub fn get_env() {
+    env::var("CARGO_FEATURE_STD").unwrap();
+}
+
+use std::fs::File;
+use std::io::{self, Read, Write};
+use std::path::Path;
+
+pub fn mymain() -> io::Result<()> {
+    let path = Path::new("hello.txt");
+    let message = "Hello, Rustaceans!";
+
+    // Writing to a file
+    {
+        let mut file = File::create(&path)?;
+        file.write_all(message.as_bytes())?;
+    }
+
+    // Reading from a file
+    {
+        let mut file = File::open(&path)?;
+        let mut contents = String::new();
+        file.read_to_string(&mut contents)?;
+        println!("File contents: {}", contents);
+    }
+
+    // Clean up
+    std::fs::remove_file(&path)?;
+
+    Ok(())
+}
 //use rs_sdk::dpp::identity::IdentityV0;
 //use rs_sdk::platform::Identity;
 
