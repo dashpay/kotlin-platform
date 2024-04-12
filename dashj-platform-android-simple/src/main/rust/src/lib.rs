372f4b562f411f540ec462d29c27fc3aa7e01e02
HashEngineering <hashengineeringsolutions@gmail.com>
2024-04-11 22:19:34 -0700

    fix: repair simple android library

--- dashj-platform-android-simple/src/main/rust/src/lib.rs
+++ dashj-platform-android-simple/src/main/rust/src/lib.rs
@@ -33,14 +33,9 @@ pub fn mymain() -> io::Result<()> {
 
     Ok(())
 }
-//use rs_sdk::dpp::identity::IdentityV0;
-//use rs_sdk::platform::Identity;
+use rs_sdk::dpp::identity::IdentityV0;
+use rs_sdk::platform::Identity;
 
-//pub fn get_default_identity() -> Identity {
-//    Identity::V0(IdentityV0::default())
-//}
-
-// use bls_signatures::PrivateKey;
-// pub fn get_secret_key() -> PrivateKey {
-//     PrivateKey::FromBytes();
-// }
+pub fn get_default_identity() -> Identity {
+   Identity::V0(IdentityV0::default())
+}
