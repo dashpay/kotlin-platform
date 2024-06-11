0ea4fc2da6dcb3354fe9d841a0b18b18dfa8cbc5
HashEngineering <hashengineeringsolutions@gmail.com>
2024-06-10 22:08:49 -0700

    feat: add ShowProfiles example

---
+++ examples/src/main/kotlin/dashj/org/platform/ShowProfiles.kt
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2020-present, Dash Core Group
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+package dashj.org.platform
+
+import org.dashj.platform.dapiclient.model.DocumentQuery
+import org.dashj.platform.dpp.document.Document
+import org.dashj.platform.sdk.Client
+import org.dashj.platform.sdk.client.ClientOptions
+import org.dashj.platform.sdk.platform.Documents
+
+class ShowProfiles {
+    companion object {
+        lateinit var sdk: Client
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            if (args.isEmpty()) {
+                println("Usage: ShowProfiles network")
+                return
+            }
+            sdk = Client(ClientOptions(network = args[0]))
+            sdk.platform.useValidNodes()
+            getDocuments()
+        }
+
+        fun getDocuments() {
+            val platform = sdk.platform
+
+            var startAt = 0
+            var documents: List<Document>? = null
+            var requests = 0
+            var queryOpts = DocumentQuery.Builder().build()
+
+            do {
+                println(queryOpts.toJSON())
+                try {
+                    documents = platform.documents.get("dashpay.profile", queryOpts)
+
+                    requests += 1
+
+                    for (doc in documents) {
+                        println(
+                            "displayName: " + doc.data["displayName"] +
+                                " (avatar: " + doc.data["avatarUrl"] +
+                                ") Identity: " + doc.ownerId
+                        )
+                        println("-> publicMessage: " + doc.data["publicMessage"])
+                        println()
+                        // println(JSONObject(doc.toJSON()).toString())
+                        println("------------------------------------------------------------")
+                    }
+
+                    startAt += Documents.DOCUMENT_LIMIT
+                    if (documents.isNotEmpty()) {
+                        queryOpts = DocumentQuery.Builder().startAfter(documents.last().id).build()
+                    }
+                } catch (e: Exception) {
+                    println("\nError retrieving results (startAt =  $startAt)")
+                    println(e.message)
+                }
+            } while (requests == 0 || documents!!.size >= 100)
+        }
+    }
+}
