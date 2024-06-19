f245a1b1d20d4be45ea104bbb54d003769222ac8
HashEngineering <hashengineeringsolutions@gmail.com>
2024-06-19 08:31:52 -0700

    tests: add dapiclient tests

---
+++ dpp/src/test/kotlin/org/dashj/platform/dapiclient/BaseTest.kt
@@ -0,0 +1,12 @@
+package org.dashj.platform.dapiclient
+
+import org.bitcoinj.utils.BriefLogFormatter
+
+open class BaseTest {
+    companion object {
+        init {
+            BriefLogFormatter.init()
+            System.loadLibrary("sdklib")
+        }
+    }
+}
---
+++ dpp/src/test/kotlin/org/dashj/platform/dapiclient/DapiClientTest.kt
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2020-present, Dash Core Team
+ *
+ * This source code is licensed under the MIT license found in the
+ * COPYING file in the root directory of this source tree.
+ */
+package org.dashj.platform.dapiclient
+
+import org.bitcoinj.params.TestNet3Params
+import org.dashj.platform.dpp.DashPlatformProtocol
+import org.junit.jupiter.api.Test
+
+class DapiClientTest : BaseTest() {
+
+    val PARAMS = TestNet3Params.get()
+    val stateRepository = StateRepositoryMock()
+    val dpp = DashPlatformProtocol(stateRepository, PARAMS)
+    val client = DapiClient(PARAMS.defaultHPMasternodeList.toList(), dpp, false)
+
+    @Test
+    fun jRPCTests() {
+        println("blocking=> hash: " + client.getBestBlockHash())
+    }
+
+    fun getMnListDiff(): Map<String, Any> {
+        val success = 0
+        do {
+            try {
+                val baseBlockHash = client.getBlockHash(0)
+                val blockHash = client.getBestBlockHash()
+                val mnListDiff = client.getMnListDiff(baseBlockHash!!, blockHash!!) as Map<String, Any>
+                return mnListDiff as Map<String, Any>
+            } catch (e: Exception) {
+                println("Error: $e")
+            }
+        } while (success == 0)
+        return mapOf()
+    }
+
+    @Test
+    fun getMnListDiffTest() {
+        println("blocking=> hash: " + client.getBestBlockHash())
+        println(getMnListDiff()["mnList"])
+    }
+}
