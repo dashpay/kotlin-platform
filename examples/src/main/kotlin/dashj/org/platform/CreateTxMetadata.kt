578e7c4d784f2285ae4fd67cbb3d9124eba5705b
HashEngineering <hashengineeringsolutions@gmail.com>
2024-06-19 14:50:05 -0700

    feat: add CreateTxMetadata example

---
+++ examples/src/main/kotlin/dashj/org/platform/CreateTxMetadata.kt
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2022-present, Dash Core Group
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+package dashj.org.platform
+
+import org.dashj.platform.contracts.wallet.TxMetadataDocument
+import org.dashj.platform.contracts.wallet.TxMetadataItem
+import org.dashj.platform.dashpay.BlockchainIdentity
+import org.dashj.platform.dpp.util.Entropy
+import org.dashj.platform.sdk.Client
+import org.dashj.platform.sdk.client.ClientOptions
+import org.dashj.platform.sdk.client.WalletOptions
+import org.dashj.platform.wallet.TxMetadata
+import org.json.JSONObject
+
+class CreateTxMetadata {
+    companion object {
+        lateinit var client: Client
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            if (args.isEmpty()) {
+                println("Usage: CreateTxMetadata network showOnly")
+                return
+            }
+            client = Client(ClientOptions(network = args[0], walletOptions = WalletOptions(DefaultIdentity(args[0]).seed)))
+            client.platform.useValidNodes()
+            createDocument(args.size >= 2 && args[1] == "true")
+        }
+
+        private fun createDocument(showOnly: Boolean) {
+            val blockchainIdentity = BlockchainIdentity(client.platform, 0, client.wallet!!, client.authenticationExtension)
+            blockchainIdentity.recoverIdentity()
+            val identity = blockchainIdentity.identity!!
+
+            val txMetadata = TxMetadata(client.platform)
+
+            if (!showOnly) {
+                val txMetadataItems = listOf(
+                    TxMetadataItem(Entropy.generateRandomBytes(32), 1, memo = "Pizza Party"),
+                    TxMetadataItem(Entropy.generateRandomBytes(32), 2, memo = "Book Store")
+                )
+
+                blockchainIdentity.publishTxMetaData(txMetadataItems, null)
+            }
+            val documents = txMetadata.get(identity.id)
+
+            println("Tx Metadata: -----------------------------------")
+            for (doc in documents) {
+                val txDoc = TxMetadataDocument(doc)
+                if (txDoc.encryptedMetadata[0] != 0.toByte()) {
+                    println(JSONObject(doc.toJSON()).toString(2))
+                    val txList = blockchainIdentity.decryptTxMetadata(txDoc, null)
+                    println("  $txList")
+                }
+            }
+        }
+    }
+}
