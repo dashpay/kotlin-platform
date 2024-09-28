fcb1ee936745ba31cb2398a2e8e914e69038d1a4
HashEngineering <hashengineeringsolutions@gmail.com>
2024-09-27 18:40:30 -0700

    fix: add one more error for using chainlocks after instant locks

--- dpp/src/main/java/org/dashj/platform/dashpay/BlockchainIdentity.kt
+++ dpp/src/main/java/org/dashj/platform/dashpay/BlockchainIdentity.kt
@@ -395,7 +395,9 @@ class BlockchainIdentity {
             try {
                 registerIdentityWithISLock(keyParameter)
             } catch (e: Exception) {
-                if (e.message?.contains("Instant lock proof signature is invalid or wasn't created recently. Pleases try chain asset lock proof instead.") == true) {
+
+                if (e.message?.contains("Instant lock proof signature is invalid or wasn't created recently. Pleases try chain asset lock proof instead.") == true ||
+                    e.message?.contains(Regex("Asset Lock proof core chain height \\d+ is higher than the current consensus core height \\d+")) == true) {
                     registerIdentityWithChainLock(keyParameter, waitForChainlock)
                 } else {
                     throw e
