25abbcc0a74e452235aa75a90c51b3cc2c86d320
HashEngineering <hashengineeringsolutions@gmail.com>
2024-10-29 13:31:52 -0700

    fix: exclude platform_mobile_sdk_destroy_dash_sdk from ignore rules

--- dash-sdk-java/src/main/cpp/clone.h
+++ dash-sdk-java/src/main/cpp/clone.h
@@ -1,20 +1,17 @@
-inline platform_mobile_put_InstantAssetLockProofFFI * clone(platform_mobile_put_InstantAssetLockProofFFI * object) {
-    return (object != nullptr) ? platform_mobile_put_InstantAssetLockProofFFI_clone(object) : nullptr;
+inline OutPoint * clone(OutPoint * object) {
+    return (object != nullptr) ? platform_mobile_put_OutPointFFI_clone(object) : nullptr;
 }
 inline platform_mobile_put_ChainAssetLockProofFFI * clone(platform_mobile_put_ChainAssetLockProofFFI * object) {
     return (object != nullptr) ? platform_mobile_put_ChainAssetLockProofFFI_clone(object) : nullptr;
 }
-inline OutPoint * clone(OutPoint * object) {
-    return (object != nullptr) ? platform_mobile_put_OutPointFFI_clone(object) : nullptr;
+inline platform_mobile_put_InstantAssetLockProofFFI * clone(platform_mobile_put_InstantAssetLockProofFFI * object) {
+    return (object != nullptr) ? platform_mobile_put_InstantAssetLockProofFFI_clone(object) : nullptr;
 }
-inline platform_mobile_data_contracts_DataContractFFI * clone(platform_mobile_data_contracts_DataContractFFI * object) {
-    return (object != nullptr) ? platform_mobile_data_contracts_DataContractFFI_clone(object) : nullptr;
+inline dpp_identity_v0_IdentityV0 * clone(dpp_identity_v0_IdentityV0 * object) {
+    return (object != nullptr) ? platform_mobile_identity_IdentityV0_clone(object) : nullptr;
 }
-inline std_collections_Map_keys_dpp_identity_identity_public_key_KeyID_values_dpp_identity_identity_public_key_IdentityPublicKey * clone(std_collections_Map_keys_dpp_identity_identity_public_key_KeyID_values_dpp_identity_identity_public_key_IdentityPublicKey * object) {
-    return (object != nullptr) ? platform_mobile_identity_std_collections_Map_keys_dpp_identity_identity_public_key_KeyID_values_dpp_identity_identity_public_key_IdentityPublicKey_clone(object) : nullptr;
-}
-inline platform_value_types_identifier_Identifier * clone(platform_value_types_identifier_Identifier * object) {
-    return (object != nullptr) ? platform_mobile_identity_Identifier_clone(object) : nullptr;
+inline dpp_identity_identity_public_key_v0_IdentityPublicKeyV0 * clone(dpp_identity_identity_public_key_v0_IdentityPublicKeyV0 * object) {
+    return (object != nullptr) ? platform_mobile_identity_IdentityPublicKeyV0_clone(object) : nullptr;
 }
 inline dpp_identity_identity_Identity * clone(dpp_identity_identity_Identity * object) {
     return (object != nullptr) ? platform_mobile_identity_Identity_clone(object) : nullptr;
@@ -22,117 +19,120 @@ inline dpp_identity_identity_Identity * clone(dpp_identity_identity_Identity * o
 inline dpp_identity_identity_public_key_IdentityPublicKey * clone(dpp_identity_identity_public_key_IdentityPublicKey * object) {
     return (object != nullptr) ? platform_mobile_identity_IdentityPublicKey_clone(object) : nullptr;
 }
-inline dpp_identity_identity_public_key_v0_IdentityPublicKeyV0 * clone(dpp_identity_identity_public_key_v0_IdentityPublicKeyV0 * object) {
-    return (object != nullptr) ? platform_mobile_identity_IdentityPublicKeyV0_clone(object) : nullptr;
+inline platform_value_types_identifier_Identifier * clone(platform_value_types_identifier_Identifier * object) {
+    return (object != nullptr) ? platform_mobile_identity_Identifier_clone(object) : nullptr;
 }
-inline dpp_identity_v0_IdentityV0 * clone(dpp_identity_v0_IdentityV0 * object) {
-    return (object != nullptr) ? platform_mobile_identity_IdentityV0_clone(object) : nullptr;
+inline std_collections_Map_keys_dpp_identity_identity_public_key_KeyID_values_dpp_identity_identity_public_key_IdentityPublicKey * clone(std_collections_Map_keys_dpp_identity_identity_public_key_KeyID_values_dpp_identity_identity_public_key_IdentityPublicKey * object) {
+    return (object != nullptr) ? platform_mobile_identity_std_collections_Map_keys_dpp_identity_identity_public_key_KeyID_values_dpp_identity_identity_public_key_IdentityPublicKey_clone(object) : nullptr;
 }
-inline dpp_document_v0_DocumentV0 * clone(dpp_document_v0_DocumentV0 * object) {
-    return (object != nullptr) ? platform_mobile_clone_DocumentV0_clone(object) : nullptr;
-}
-inline std_collections_Map_keys_String_values_platform_value_Value * clone(std_collections_Map_keys_String_values_platform_value_Value * object) {
-    return (object != nullptr) ? platform_mobile_clone_std_collections_Map_keys_String_values_platform_value_Value_clone(object) : nullptr;
-}
-inline platform_value_Hash256 * clone(platform_value_Hash256 * object) {
-    return (object != nullptr) ? platform_mobile_clone_Hash256_clone(object) : nullptr;
-}
-inline dpp_voting_votes_resource_vote_v0_ResourceVoteV0 * clone(dpp_voting_votes_resource_vote_v0_ResourceVoteV0 * object) {
-    return (object != nullptr) ? platform_mobile_clone_ResourceVoteV0_clone(object) : nullptr;
-}
-inline Arr_u8_36 * clone(Arr_u8_36 * object) {
-    return (object != nullptr) ? platform_mobile_clone_Arr_u8_36_clone(object) : nullptr;
-}
-inline dpp_prelude_TimestampMillis * clone(dpp_prelude_TimestampMillis * object) {
-    return (object != nullptr) ? platform_mobile_clone_prelude_TimestampMillis_clone(object) : nullptr;
-}
-inline drive_proof_verifier_types_Contenders * clone(drive_proof_verifier_types_Contenders * object) {
-    return (object != nullptr) ? platform_mobile_clone_Contenders_clone(object) : nullptr;
-}
-inline drive_proof_verifier_types_Voter * clone(drive_proof_verifier_types_Voter * object) {
-    return (object != nullptr) ? platform_mobile_clone_Voter_clone(object) : nullptr;
-}
-inline dpp_voting_vote_choices_resource_vote_choice_ResourceVoteChoice * clone(dpp_voting_vote_choices_resource_vote_choice_ResourceVoteChoice * object) {
-    return (object != nullptr) ? platform_mobile_clone_ResourceVoteChoice_clone(object) : nullptr;
-}
-inline Arr_u8_32 * clone(Arr_u8_32 * object) {
-    return (object != nullptr) ? platform_mobile_clone_Arr_u8_32_clone(object) : nullptr;
-}
-inline dpp_voting_vote_polls_VotePoll * clone(dpp_voting_vote_polls_VotePoll * object) {
-    return (object != nullptr) ? platform_mobile_clone_VotePoll_clone(object) : nullptr;
-}
-inline drive_proof_verifier_types_Voters * clone(drive_proof_verifier_types_Voters * object) {
-    return (object != nullptr) ? platform_mobile_clone_Voters_clone(object) : nullptr;
-}
-inline drive_proof_verifier_types_ContestedResource * clone(drive_proof_verifier_types_ContestedResource * object) {
-    return (object != nullptr) ? platform_mobile_clone_ContestedResource_clone(object) : nullptr;
-}
-inline dpp_voting_contender_structs_contender_ContenderWithSerializedDocument * clone(dpp_voting_contender_structs_contender_ContenderWithSerializedDocument * object) {
-    return (object != nullptr) ? platform_mobile_clone_ContenderWithSerializedDocument_clone(object) : nullptr;
-}
-inline platform_value_value_map_ValueMap * clone(platform_value_value_map_ValueMap * object) {
-    return (object != nullptr) ? platform_mobile_clone_ValueMap_clone(object) : nullptr;
-}
-inline platform_value_Value * clone(platform_value_Value * object) {
-    return (object != nullptr) ? platform_mobile_clone_Value_clone(object) : nullptr;
-}
-inline dpp_prelude_Revision * clone(dpp_prelude_Revision * object) {
-    return (object != nullptr) ? platform_mobile_clone_Revision_clone(object) : nullptr;
-}
-inline drive_proof_verifier_types_ContestedResources * clone(drive_proof_verifier_types_ContestedResources * object) {
-    return (object != nullptr) ? platform_mobile_clone_ContestedResources_clone(object) : nullptr;
-}
-inline Vec_u8 * clone(Vec_u8 * object) {
-    return (object != nullptr) ? platform_mobile_clone_Vec_u8_clone(object) : nullptr;
-}
-inline drive_query_ordering_OrderClause * clone(drive_query_ordering_OrderClause * object) {
-    return (object != nullptr) ? platform_mobile_clone_OrderClause_clone(object) : nullptr;
-}
-inline std_collections_BTreeSet_drive_proof_verifier_types_Voter * clone(std_collections_BTreeSet_drive_proof_verifier_types_Voter * object) {
-    return (object != nullptr) ? platform_mobile_clone_std_collections_BTreeSet_drive_proof_verifier_types_Voter_clone(object) : nullptr;
-}
-inline dpp_identity_identity_public_key_KeyID * clone(dpp_identity_identity_public_key_KeyID * object) {
-    return (object != nullptr) ? platform_mobile_clone_KeyID_clone(object) : nullptr;
-}
-inline dpp_prelude_CoreBlockHeight * clone(dpp_prelude_CoreBlockHeight * object) {
-    return (object != nullptr) ? platform_mobile_clone_CoreBlockHeight_clone(object) : nullptr;
-}
-inline drive_query_conditions_WhereClause * clone(drive_query_conditions_WhereClause * object) {
-    return (object != nullptr) ? platform_mobile_clone_WhereClause_clone(object) : nullptr;
-}
-inline Arr_u8_20 * clone(Arr_u8_20 * object) {
-    return (object != nullptr) ? platform_mobile_clone_Arr_u8_20_clone(object) : nullptr;
-}
-inline dpp_voting_votes_Vote * clone(dpp_voting_votes_Vote * object) {
-    return (object != nullptr) ? platform_mobile_clone_Vote_clone(object) : nullptr;
-}
-inline dpp_voting_contender_structs_contender_v0_ContenderWithSerializedDocumentV0 * clone(dpp_voting_contender_structs_contender_v0_ContenderWithSerializedDocumentV0 * object) {
-    return (object != nullptr) ? platform_mobile_clone_ContenderWithSerializedDocumentV0_clone(object) : nullptr;
-}
-inline Tuple_dpp_prelude_TimestampMillis_Vec_dpp_voting_vote_polls_VotePoll * clone(Tuple_dpp_prelude_TimestampMillis_Vec_dpp_voting_vote_polls_VotePoll * object) {
-    return (object != nullptr) ? platform_mobile_clone_Tuple_dpp_prelude_TimestampMillis_Vec_dpp_voting_vote_polls_VotePoll_clone(object) : nullptr;
-}
-inline Vec_platform_value_Value * clone(Vec_platform_value_Value * object) {
-    return (object != nullptr) ? platform_mobile_clone_Vec_Value_clone(object) : nullptr;
-}
-inline drive_proof_verifier_types_VotePollsGroupedByTimestamp * clone(drive_proof_verifier_types_VotePollsGroupedByTimestamp * object) {
-    return (object != nullptr) ? platform_mobile_clone_VotePollsGroupedByTimestamp_clone(object) : nullptr;
+inline platform_mobile_data_contracts_DataContractFFI * clone(platform_mobile_data_contracts_DataContractFFI * object) {
+    return (object != nullptr) ? platform_mobile_data_contracts_DataContractFFI_clone(object) : nullptr;
 }
 inline dpp_voting_votes_resource_vote_ResourceVote * clone(dpp_voting_votes_resource_vote_ResourceVote * object) {
     return (object != nullptr) ? platform_mobile_clone_ResourceVote_clone(object) : nullptr;
 }
-inline dpp_prelude_BlockHeight * clone(dpp_prelude_BlockHeight * object) {
-    return (object != nullptr) ? platform_mobile_clone_BlockHeight_clone(object) : nullptr;
+inline dpp_voting_votes_resource_vote_v0_ResourceVoteV0 * clone(dpp_voting_votes_resource_vote_v0_ResourceVoteV0 * object) {
+    return (object != nullptr) ? platform_mobile_clone_ResourceVoteV0_clone(object) : nullptr;
+}
+inline platform_value_Value * clone(platform_value_Value * object) {
+    return (object != nullptr) ? platform_mobile_clone_Value_clone(object) : nullptr;
+}
+inline dpp_voting_vote_choices_resource_vote_choice_ResourceVoteChoice * clone(dpp_voting_vote_choices_resource_vote_choice_ResourceVoteChoice * object) {
+    return (object != nullptr) ? platform_mobile_clone_ResourceVoteChoice_clone(object) : nullptr;
+}
+inline drive_proof_verifier_types_Voter * clone(drive_proof_verifier_types_Voter * object) {
+    return (object != nullptr) ? platform_mobile_clone_Voter_clone(object) : nullptr;
+}
+inline Arr_u8_32 * clone(Arr_u8_32 * object) {
+    return (object != nullptr) ? platform_mobile_clone_Arr_u8_32_clone(object) : nullptr;
+}
+inline Tuple_dpp_prelude_TimestampMillis_Vec_dpp_voting_vote_polls_VotePoll * clone(Tuple_dpp_prelude_TimestampMillis_Vec_dpp_voting_vote_polls_VotePoll * object) {
+    return (object != nullptr) ? platform_mobile_clone_Tuple_dpp_prelude_TimestampMillis_Vec_dpp_voting_vote_polls_VotePoll_clone(object) : nullptr;
+}
+inline platform_value_Hash256 * clone(platform_value_Hash256 * object) {
+    return (object != nullptr) ? platform_mobile_clone_Hash256_clone(object) : nullptr;
 }
 inline dpp_voting_vote_polls_contested_document_resource_vote_poll_ContestedDocumentResourceVotePoll * clone(dpp_voting_vote_polls_contested_document_resource_vote_poll_ContestedDocumentResourceVotePoll * object) {
     return (object != nullptr) ? platform_mobile_clone_ContestedDocumentResourceVotePoll_clone(object) : nullptr;
 }
+inline dpp_voting_contender_structs_contender_ContenderWithSerializedDocument * clone(dpp_voting_contender_structs_contender_ContenderWithSerializedDocument * object) {
+    return (object != nullptr) ? platform_mobile_clone_ContenderWithSerializedDocument_clone(object) : nullptr;
+}
+inline drive_proof_verifier_types_ContestedResource * clone(drive_proof_verifier_types_ContestedResource * object) {
+    return (object != nullptr) ? platform_mobile_clone_ContestedResource_clone(object) : nullptr;
+}
 inline dpp_identity_identity_public_key_TimestampMillis * clone(dpp_identity_identity_public_key_TimestampMillis * object) {
     return (object != nullptr) ? platform_mobile_clone_TimestampMillis_clone(object) : nullptr;
+}
+inline dpp_identity_identity_public_key_KeyID * clone(dpp_identity_identity_public_key_KeyID * object) {
+    return (object != nullptr) ? platform_mobile_clone_KeyID_clone(object) : nullptr;
+}
+inline drive_query_ordering_OrderClause * clone(drive_query_ordering_OrderClause * object) {
+    return (object != nullptr) ? platform_mobile_clone_OrderClause_clone(object) : nullptr;
+}
+inline dpp_prelude_BlockHeight * clone(dpp_prelude_BlockHeight * object) {
+    return (object != nullptr) ? platform_mobile_clone_BlockHeight_clone(object) : nullptr;
+}
+inline dpp_prelude_Revision * clone(dpp_prelude_Revision * object) {
+    return (object != nullptr) ? platform_mobile_clone_Revision_clone(object) : nullptr;
+}
+inline platform_value_value_map_ValueMap * clone(platform_value_value_map_ValueMap * object) {
+    return (object != nullptr) ? platform_mobile_clone_ValueMap_clone(object) : nullptr;
 }
 inline dpp_document_Document * clone(dpp_document_Document * object) {
     return (object != nullptr) ? platform_mobile_clone_Document_clone(object) : nullptr;
 }
+inline dpp_voting_contender_structs_contender_v0_ContenderWithSerializedDocumentV0 * clone(dpp_voting_contender_structs_contender_v0_ContenderWithSerializedDocumentV0 * object) {
+    return (object != nullptr) ? platform_mobile_clone_ContenderWithSerializedDocumentV0_clone(object) : nullptr;
+}
+inline dpp_voting_vote_polls_VotePoll * clone(dpp_voting_vote_polls_VotePoll * object) {
+    return (object != nullptr) ? platform_mobile_clone_VotePoll_clone(object) : nullptr;
+}
+inline drive_proof_verifier_types_Contenders * clone(drive_proof_verifier_types_Contenders * object) {
+    return (object != nullptr) ? platform_mobile_clone_Contenders_clone(object) : nullptr;
+}
+inline std_collections_Map_keys_String_values_platform_value_Value * clone(std_collections_Map_keys_String_values_platform_value_Value * object) {
+    return (object != nullptr) ? platform_mobile_clone_std_collections_Map_keys_String_values_platform_value_Value_clone(object) : nullptr;
+}
+inline dpp_document_v0_DocumentV0 * clone(dpp_document_v0_DocumentV0 * object) {
+    return (object != nullptr) ? platform_mobile_clone_DocumentV0_clone(object) : nullptr;
+}
+inline drive_proof_verifier_types_ContestedResources * clone(drive_proof_verifier_types_ContestedResources * object) {
+    return (object != nullptr) ? platform_mobile_clone_ContestedResources_clone(object) : nullptr;
+}
+inline dpp_voting_votes_Vote * clone(dpp_voting_votes_Vote * object) {
+    return (object != nullptr) ? platform_mobile_clone_Vote_clone(object) : nullptr;
+}
 inline drive_query_conditions_WhereOperator * clone(drive_query_conditions_WhereOperator * object) {
     return (object != nullptr) ? platform_mobile_clone_WhereOperator_clone(object) : nullptr;
+}
+inline std_collections_BTreeSet_drive_proof_verifier_types_Voter * clone(std_collections_BTreeSet_drive_proof_verifier_types_Voter * object) {
+    return (object != nullptr) ? platform_mobile_clone_std_collections_BTreeSet_drive_proof_verifier_types_Voter_clone(object) : nullptr;
+}
+inline Arr_u8_20 * clone(Arr_u8_20 * object) {
+    return (object != nullptr) ? platform_mobile_clone_Arr_u8_20_clone(object) : nullptr;
+}
+inline drive_proof_verifier_types_Voters * clone(drive_proof_verifier_types_Voters * object) {
+    return (object != nullptr) ? platform_mobile_clone_Voters_clone(object) : nullptr;
+}
+inline drive_proof_verifier_types_VotePollsGroupedByTimestamp * clone(drive_proof_verifier_types_VotePollsGroupedByTimestamp * object) {
+    return (object != nullptr) ? platform_mobile_clone_VotePollsGroupedByTimestamp_clone(object) : nullptr;
+}
+inline Arr_u8_36 * clone(Arr_u8_36 * object) {
+    return (object != nullptr) ? platform_mobile_clone_Arr_u8_36_clone(object) : nullptr;
+}
+inline Vec_u8 * clone(Vec_u8 * object) {
+    return (object != nullptr) ? platform_mobile_clone_Vec_u8_clone(object) : nullptr;
+}
+inline drive_query_conditions_WhereClause * clone(drive_query_conditions_WhereClause * object) {
+    return (object != nullptr) ? platform_mobile_clone_WhereClause_clone(object) : nullptr;
+}
+inline dpp_prelude_CoreBlockHeight * clone(dpp_prelude_CoreBlockHeight * object) {
+    return (object != nullptr) ? platform_mobile_clone_CoreBlockHeight_clone(object) : nullptr;
+}
+inline Vec_platform_value_Value * clone(Vec_platform_value_Value * object) {
+    return (object != nullptr) ? platform_mobile_clone_Vec_Value_clone(object) : nullptr;
+}
+inline dpp_prelude_TimestampMillis * clone(dpp_prelude_TimestampMillis * object) {
+    return (object != nullptr) ? platform_mobile_clone_prelude_TimestampMillis_clone(object) : nullptr;
 }
