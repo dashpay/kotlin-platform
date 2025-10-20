2579d9aa3dbe23bac866cdfff86ea3522e5e23c0
HashEngineering <hashengineeringsolutions@gmail.com>
2025-10-20 11:29:54 -0700

    tests: update DataContractTest

--- dash-sdk-java/src/test/java/org/dashj/platform/sdk/DataContractTest.java
+++ dash-sdk-java/src/test/java/org/dashj/platform/sdk/DataContractTest.java
@@ -26,6 +26,20 @@ public class DataContractTest extends SdkBaseTest {
     }
 
     @Test
+    public void fetchDashPayDataContractTest() throws Exception {
+        Identifier contractId = new Identifier(dashPayContractId);
+        Result<Optional<DataContract>, String> result = dashsdk.platformMobileDataContractsFetchDataContract(
+                sdk, contractId);
+
+        Optional<DataContract> dataContractResult = result.unwrap();
+        assertTrue(dataContractResult.isPresent());
+        DataContract dataContract = dataContractResult.get();
+        assertArrayEquals(dashPayContractId, dataContract.getId().get_0().get_0());
+        assertEquals(3, dataContract.getDoc_types().size());
+        dataContract.getDoc_types().forEach(System.out::println);
+    }
+
+    @Test
     public void fetchWalletUtilsDataContractTest() throws Exception {
         Identifier contractId = new Identifier(walletUtilsContracId);
         Result<Optional<DataContract>, String> result = dashsdk.platformMobileDataContractsFetchDataContract(
@@ -35,8 +49,9 @@ public class DataContractTest extends SdkBaseTest {
         assertTrue(dataContractResult.isPresent());
         DataContract dataContract = dataContractResult.get();
         assertArrayEquals(walletUtilsContracId, dataContract.getId().get_0().get_0());
-        assertEquals(3, dataContract.getDoc_types().size());
+        assertEquals(1, dataContract.getDoc_types().size());
         assertTrue("didn't find txMetadata", dataContract.getDoc_types().stream().allMatch(type -> type.equals("txMetadata")));
+        dataContract.getDoc_types().forEach(System.out::println);
     }
 
     @Test
